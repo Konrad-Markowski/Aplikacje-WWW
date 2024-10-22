4878c20b66f9a5c8fdf3a77f71783099aed7e581
Konrad-Markowski <k.markowski2002@gmail.com>
2024-10-22 17:34:36 +0200

    update lab3

---
+++ lab4/zad1.php
@@ -0,0 +1,8 @@
+<?php
+    $nr_indeksu = '169334';
+    $nrGrupy = 'ISI 3';
+    echo 'Konrad Markowski '.$nr_indeksu.'grupa: '.$nrGrupy.'<br><br>';
+
+    echo 'Zastosowanie metody include() <br>';
+
+?>
