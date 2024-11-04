8061ce76559c3159b667631ef531b011d3da0a08
Konrad-Markowski <k.markowski2002@gmail.com>
2024-11-04 21:55:44 +0100

    add files

--- lab4/zad1.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $nr_indeksu = '169334';
-    $nrGrupy = 'ISI 3';
-    echo 'Konrad Markowski '.$nr_indeksu.'grupa: '.$nrGrupy.'<br><br>';
-
-    echo 'Zastosowanie metody include() <br>';
-
-?>
